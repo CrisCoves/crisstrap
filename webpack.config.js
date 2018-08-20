9d25f5eedbc6ddf5492bb13f7081b0cdf9c77538
Cristina Coves Mora <kylie_212@hotmail.com>
2018-08-20 13:40:24 +0200

    [+TASK]project setup: add 'webpack.config.js' file

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+const path = require('path');
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist/")
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: "babel",
+                exclude: path.join(__dirname, "node_modules")
+            }
+        ]
+    }
+}
